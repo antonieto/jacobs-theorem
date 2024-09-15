b71e8eb158ff3dda30d99f5cce28439c35bc9754
Int-Angel <padilla.angel88@gmail.com>
2024-09-14 18:53:34 -0600

    AI symbol component

--- jacobs-theorem/app/_layout.tsx
+++ jacobs-theorem/app/_layout.tsx
@@ -1,10 +1,4 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useCallback, useEffect } from "react";
 import { VoiceProvider } from "@humeai/voice-react";
@@ -13,6 +7,7 @@ import AIChat from "@/components/AIChat";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { HumeClient, getAudioStream, checkForAudioTracks } from "hume";
+import AISymbol from "@/components/AISymbol";
 
 const API_KEY = "";
 const SECRET_KEY = "";
@@ -54,13 +49,8 @@ export default function RootLayout() {
 
   return (
     <VoiceProvider auth={{ type: "apiKey", value: API_KEY }}>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        {/* <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack> */}
-        <AIChat />
-      </ThemeProvider>
+      {/*<AIChat/>*/}
+      <AISymbol isTalking={true} />
     </VoiceProvider>
   );
 }
