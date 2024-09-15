7fb8f29cde393c046783a865d49655fd6810339c
Antonio Chaires <tony_monroe1212@hotmail.com>
2024-09-14 23:19:41 -0600

    add json api call

--- jacobs-theorem/app/_layout.tsx
+++ jacobs-theorem/app/_layout.tsx
@@ -1,6 +1,6 @@
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { VoiceProvider } from "@humeai/voice-react";
 import "react-native-reanimated";
 import AIChat from "@/components/AIChat";
@@ -17,22 +17,13 @@ export default function RootLayout() {
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
-  const cb = useCallback(async () => {
-    try {
-      console.log("NAVIGATOR: ", navigator);
-      const stream = await getAudioStream();
-      const tracks = await checkForAudioTracks(stream);
-      console.log(tracks);
-    } catch (error) {
-      console.error(error);
-    }
-  }, []);
+  const [conversation, setConversation] = useState<{ sender: 'user' | 'assistant', message: string }[]>([]);
+
 
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
     }
-    cb();
   }, [loaded]);
 
   if (!loaded) {
@@ -46,6 +37,25 @@ export default function RootLayout() {
         value: process.env.EXPO_PUBLIC_HUME_API_KEY!,
       }}
       configId="d8db284c-7c04-433c-bc42-801c5a974cdb"
+      onMessage={(message) => {
+        if (message.type === 'assistant_end') {
+          // TODO: add parse KYC conversation API call here
+          // Parse conversation to plain text
+          const plainText = conversation.reduce((acc, curr) => acc + `${curr.sender === 'user' ? 'User: ' : 'Assistant: '}${curr.message}\n`, '');
+          console.log(plainText);
+        }
+        if (message.type === 'assistant_message' || message.type === 'user_message') {
+          setConversation(prev => [...prev, { sender: message.type === 'assistant_message' ? 'assistant' : 'user', message: message.message.content ?? '' }]);
+        }
+      }}
+      onClose={(ev) => {
+        console.log(ev);
+        // TODO: add parse KYC conversation API call here
+        if (ev.wasClean) {
+          const plainText = conversation.reduce((acc, curr) => acc + `${curr.sender === 'user' ? 'User: ' : 'Assistant: '}${curr.message}\n`, '');
+          console.log(plainText);
+        }
+      }}
     >
       <AIChat />
     </VoiceProvider>
