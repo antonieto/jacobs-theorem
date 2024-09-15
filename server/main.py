a087183e194c20d317b0be1808b13dcac44933b6
CarlosCRG19 <carloscrg19@gmail.com>
2024-09-15 01:02:33 -0600

    update fastapi cors

--- server/main.py
+++ server/main.py
@@ -5,8 +5,17 @@ from typing import Dict
 import openai
 from dotenv import load_dotenv
 import json
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 load_dotenv()
 
